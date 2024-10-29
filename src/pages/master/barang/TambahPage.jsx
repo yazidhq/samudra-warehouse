4c9676ab8a0abffbaae3b3cad3e3692361fd6272
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-10-29 16:09:05 +0700

    update: form create new product

--- src/pages/master/barang/TambahPage.jsx
+++ src/pages/master/barang/TambahPage.jsx
@@ -12,7 +12,55 @@ const MasterPage = () => {
           <h1 className="h4">Form Item</h1>
         </div>
         <hr />
-        <div className="d-flex justify-content-between mb-3">
+        <div class="mb-3 row">
+          <div class="col-2">
+            <label class="form-label mt-2">Kode Barang</label>
+          </div>
+          <div class="col-10">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <div class="col-2">
+            <label class="form-label mt-2">Jenis Barang</label>
+          </div>
+          <div class="col-10">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <div class="col-2">
+            <label class="form-label mt-2">Nama Barang</label>
+          </div>
+          <div class="col-10">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <div class="col-2">
+            <label class="form-label mt-2">Satuan Barang</label>
+          </div>
+          <div class="col-10">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <div class="col-2">
+            <label class="form-label mt-2">Ukuran Barang</label>
+          </div>
+          <div class="col-10">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <div class="col-2">
+            <label class="form-label mt-2">stok Barang</label>
+          </div>
+          <div class="col-10">
+            <input type="number" min={"0"} class="form-control" />
+          </div>
+        </div>
+        <div className="d-flex justify-content-between mb-3 mt-5">
           <Link to={"/barang"} className="text-decoration-none text-red">
             <span style={{ color: "red" }}>
               <FaArrowLeft
